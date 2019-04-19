808b28cbbf3f66f50ee51164cfdffa0f67d99189
Johnnybar <johnnybareket@gmail.com>
2019-04-19 21:17:32 +0200

    changed aws bucket images to random

--- src/friends.js
+++ src/friends.js
@@ -27,13 +27,14 @@ class Friends extends React.Component {
     this.props.dispatch(allFriendRequests());
   }
   render() {
+    const arr = ["https://cdn.pixabay.com/photo/2016/03/31/23/37/monster-1297726_960_720.png","https://orig00.deviantart.net/6cab/f/2014/114/9/2/monstruo_vector_move_3_by_alba_r_luque-d7frqes.gif","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrkWU36GHqTAy8sA-FsMWTInoz6WfMTo2g-UKbRFFgX6XbuC-5","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTW7sCc3l0dlFO9ywORyVtZ-wwSLsTK_1WPEr92HdAKW6_Itfd1","https://cdn.pixabay.com/photo/2016/03/22/17/28/transparent-background-1273346_960_720.png","https://farm6.staticflickr.com/5303/5687100580_3e66d28f07_b.jpg","https://cdn.pixabay.com/photo/2015/09/22/08/10/monster-951232_960_720.png","https://cdn.pixabay.com/photo/2016/03/31/20/16/alien-1295644_960_720.png","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQILwAQkyJepN6A1v65oN9V2FBfCzlP3U1kHlWIE5Iw4tjLVvND2g","https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSvRIoDJcijiD0AX35hdJN-r3hyXj11Qje3RzydXLdSDIqhpsck"]
     const pendingFriends = this.props.pendingFriends;
     const acceptedFriends = this.props.acceptedFriends
     if (!pendingFriends) {
       return null
     }
     for (var i = 0; i < pendingFriends.length; i++) {
-      pendingFriends[i].imgurl = "https://picsum.photos/200"
+      pendingFriends[i].imgurl = arr[i]
     }
     const pendingFriendsList = pendingFriends.map(pending => <div>
       <div>
